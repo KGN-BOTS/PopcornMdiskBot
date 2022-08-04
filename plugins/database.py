9d6d20d3d55303e5fce5c909f143d26989ffea64
Akshay-Chand <107416405+Akshay-Chand@users.noreply.github.com>
2022-08-04 11:46:54 +0530

    Delete plugins directory

--- plugins/database.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-from config import MONGODB, DATABASE_NAME, COLLECTION_NAME
-import ssl
-
-URI = MONGODB
-db = pymongo.MongoClient(URI, ssl_cert_reqs=ssl.CERT_NONE)
-client = db[DATABASE_NAME]
-collection = client[COLLECTION_NAME]
-
