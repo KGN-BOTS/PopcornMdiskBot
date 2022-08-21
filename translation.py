35c4de34b1219cd8bb2d646ebd9b12e6044d20f8
Amit Jnagra <110877200+Amit00999@users.noreply.github.com>
2022-08-21 08:38:50 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,4 +1,4 @@
-START_MESSAGE = "**Hey! {}😁 \n\n I'm Mdisk Searcher bot. 🤖 \n\n I Can Search!🔍 What You Want?😜 \n\n Made With ❤ By @Iamit09.**"
+START_MESSAGE = "**Hey! {}😁 \n\n I'm Mdisk Searcher bot. 🤖 \n\n I Can Search!🔍 What You Want?😜 \n\n Made With ❤ By @Iamit09 .**"
 
 NO_RESULTS_FOUND = '''**No Results Found for {}❗️ \n\n 1.please Write Correct spelling From Google \n\n 2.Do Not add Season or Episode \n\n 3.Do Not add languages or Year \n\n 4.Write only correct movie/Series Name \n\n 5.if Movie Not found Then Request to Admin
 
